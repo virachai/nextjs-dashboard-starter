8131f07a6d54f0da7508d8e53d004e09fd6e3419
Virachai Wongsena <wvirachai@gmail.com>
2025-01-13 18:50:53 +0700

    Recent Revenue Chart

---
+++ app/dashboard/template.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    setCurrentTime(new Date());
+  }, []);
+  return (
+    <div>
+      <div className="hidden">
+        {/* Display current time */}
+        <span>Current Time: {currentTime.toLocaleTimeString()}</span>
+      </div>
+      {children}
+    </div>
+  );
+}
