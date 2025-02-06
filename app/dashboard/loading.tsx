fc70e21548e5503f64db7ea6d7bb3b4442bdf5e0
Virachai Wongsena <wvirachai@gmail.com>
2025-02-06 10:33:54 +0700

    remove dashboard

--- app/dashboard/loading.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardSkeleton from '@/app/ui/skeletons';
-
-export default function Loading() {
-  return <DashboardSkeleton />;
-}
-
-// export default function Loading() {
-//   return <div>Loading...</div>;
-// }
